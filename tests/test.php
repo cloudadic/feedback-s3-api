9759f5045af12b844b231581d0d0b88e2be4a687
Mir Adnan <miradnanali@gmail.com>
2017-08-16 19:31:51 +0530

    Update test.php

--- tests/test.php
+++ tests/test.php
@@ -3,6 +3,6 @@
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/FeedbackUpload.php';
 require __DIR__ . '/S3Bucket.php';
-$feedback = new cloudadic\feedback\FeedbackUpload(new cloudadic\feedback\S3Bucket('ACCESS_KEY', 'SECRET KEY', 'pubema', 'us-east-1', '2006-03-01'));
+$feedback = new cloudadic\feedback\FeedbackUpload(new cloudadic\feedback\S3Bucket('ACCESS_KEY', 'SECRET KEY', 'S3 BUCKET', 'us-east-1', '2006-03-01'));
 $response = $feedback->UploadImage('/home/ziwid/Downloads/062aa016159b28294f8229cbbd0f602f.jpg', 'test/062aa016159b28294f8229cbbd0f602f.jpg');
 print_r($response);
